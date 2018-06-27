abbea6f75cb2d03261be43b88561d6e5045debae
Reginaldo <regisrm7@gmail.com>
2018-06-27 18:04:58 -0300

    Atualização

--- app/src/main/java/reginaldomagalhaes/wordpress/com/alcooolougasolina/MainActivity.java
+++ app/src/main/java/reginaldomagalhaes/wordpress/com/alcooolougasolina/MainActivity.java
@@ -2,12 +2,47 @@ package reginaldomagalhaes.wordpress.com.alcooolougasolina;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText precoGasolina;
+    private EditText precoAlcool;
+    private Button botaoverificar;
+    private TextView textoresultdo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        precoGasolina = (EditText) findViewById(R.id.caixaPrecoGasolina_id);
+        precoAlcool = (EditText) findViewById(R.id.caixaPrecoAlcool_id);
+        botaoverificar = (Button) findViewById(R.id.botaoVerificar_id);
+        textoresultdo= (TextView) findViewById(R.id.textoSaida_id);
+        botaoverificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String textoPrecoGasolina = precoGasolina.getText().toString();
+                String textoPrecoAlcool = precoAlcool.getText().toString();
+                Double valorGasolina = Double.parseDouble(textoPrecoGasolina);
+                Double valorAlcool = Double.parseDouble(textoPrecoAlcool);
+                Double resultado = valorAlcool / valorGasolina;
+
+                if (resultado >= 0.7) {
+                    textoresultdo.setText("É melhor usar gasolina, pois é maior que 70%");
+                } else {
+                    textoresultdo.setText("É melhor usar alcool, pois é menor que 70%");
+                }
+            }
+
+
+        });
+
+
+
+    };
 }
